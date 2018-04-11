9830c81ae7147d9e39f6e880b5add8171738547d
gene <gene@js-tech.tw>
2018-04-11 11:51:18 +0800

    範例測試中增加簡易的「加法」測試，說明 Arrange、Act，與 Assert

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -27,6 +27,25 @@ class ExampleTest extends BaseTestCase
      */
     public function testBasicTest()
     {
-        $this->assertTrue(true);
+        // Arrange：定義要使用的資料
+        $originNumber = 1;
+        $addedNumber = 2;
+        $expectResult = 3;
+
+        // Act：定義要執行的動作
+        $actingResult = $this->add($originNumber, $addedNumber);
+
+        // Assert：斷言執行的結果
+        $this->assertEquals($expectResult, $actingResult);
+    }
+
+    /**
+     * add a and b
+     * @param int $a
+     * @param int $b
+     */
+    public function add(int $a, int $b)
+    {
+        return $a + $b;
     }
 }
